93b21c5f970af0172b2b4e4e0c24989b807ec5c5
Subham Chakraborty <subhamnation123@gmail.com>
2026-03-13 20:47:28 +0530

    Class and Object basics done

---
+++ oops_1.py
@@ -0,0 +1,27 @@
+# Initiate a class
+
+class employee:
+    # special method / magic method / dunder method - Constructor 
+    def __init__(self):
+        print("Stated executing attribute/data")
+        self.id = 123
+        self.salary = 50000 
+        self.designation = "SDE"
+        print("attributes/data have been initiated")
+
+
+    def travel(self, destination):
+        print("This travel method was called manually")
+        print(f"Employee is now travelling to {destination}")
+
+
+# Creating an object / Instance of the class 
+
+sam = employee()
+
+# print(sam.salary)
+# print(sam.id)
+
+# calling a method
+sam.travel("Kerala")
+print(type(sam))
